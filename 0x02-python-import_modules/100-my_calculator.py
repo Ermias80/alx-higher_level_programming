5476429770276f486d7f38f8f3053b73b910fd94
Ermias80 <ermiasmaz8069@gmail.com>
2024-03-14 13:34:13 -0700

    all

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     """Handle basic arithmetic operations."""
-import sys
-from calculator_1 import add, sub, mul, div
-
-if len(sys.argv) != 4:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    sys.exit(1)
+    from calculator_1 import add, sub, mul, div
+    import sys
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
 
 a = int(sys.argv[1])
 operator = sys.argv[2]
