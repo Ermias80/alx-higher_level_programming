ca895c3d6b2226cb040d1141dd0d6ae778351a8b
Ermias80 <ermiasmaz8069@gmail.com>
2024-03-14 13:09:49 -0700

    all

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """Handle basic arithmetic operations."""
+import sys
+from calculator_1 import add, sub, mul, div
+
+if len(sys.argv) != 4:
+    sys.exit(1)
+
+a = int(sys.argv[1])
+operator = sys.argv[2]
+b = int(sys.argv[3])
+
+if operator == '+':
+    result = add(a, b)
+elif operator == '-':
+    result = sub(a, b)
+elif operator == '*':
+    result = mul(a, b)
+elif operator == '/':
+    result = div(a, b)
+else:
+    print("Unknown operator. Available operators: +, -, * and /")
+    sys.exit(1)
+
+print("{} {} {} = {}".format(a, operator, b, result))
+
