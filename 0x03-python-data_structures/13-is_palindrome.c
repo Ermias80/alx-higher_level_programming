0fd431783fd04d3504b16dbbbee029a0eec2ccc2
Ermias80 <ermiasmaz8069@gmail.com>
2024-03-18 14:43:35 -0700

    all

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,53 +1,72 @@
 #include "lists.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
-*add_nodeint - adds a new node at the beginning of a listint_t list
-*@head: head of listint_t
-*@n: int to add in listint_t list
-*Return: address of the new element, or NULL if it failed
-*/
-listint_t *add_nodeint(listint_t **head, const int n)
-{
-	listint_t *new;
-
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	new->next = *head;
-	*head = new;
-	return (new);
-}
-/**
-*is_palindrome - identify if a syngle linked list is palindrome
-*@head: head of listint_t
-*Return: 1 if it is palindrome else 0
-*/
+ * is_palindrome - Identifies if a singly linked list is palindrome
+ * @head: Pointer to the head of listint_t
+ * Return: 1 if it is palindrome else 0
+ */
 int is_palindrome(listint_t **head)
 {
-	listint_t *head2 = *head;
-	listint_t *aux = NULL, *aux2 = NULL;
+	listint_t *slow = *head;
+	listint_t *fast = *head;
+	listint_t *prev_slow = NULL;
+	listint_t *second_half = NULL;
+	listint_t *mid = NULL;
+	int is_pal = 1;
 
-	if (*head == NULL || head2->next == NULL)
+	if (*head == NULL || (*head)->next == NULL)
 		return (1);
-	while (head2 != NULL)
+
+	while (fast && fast->next)
 	{
-		add_nodeint(&aux, head2->n);
-		head2 = head2->next;
+		fast = fast->next->next;
+		prev_slow = slow;
+		slow = slow->next;
 	}
-	aux2 = aux;
-	while (*head != NULL)
+
+	if (fast != NULL)
 	{
-		if ((*head)->n != aux2->n)
+		mid = slow;
+		slow = slow->next;
+	}
+
+	while (slow != NULL)
+	{
+		listint_t *temp = slow->next;
+		slow->next = second_half;
+		second_half = slow;
+		slow = temp;
+	}
+
+	while (second_half != NULL)
+	{
+		if ((*head)->n != second_half->n)
 		{
-			free_listint(aux);
-			return (0);
+			is_pal = 0;
+			break;
 		}
 		*head = (*head)->next;
-		aux2 = aux2->next;
+		second_half = second_half->next;
 	}
-	free_listint(aux);
-	return (1);
+
+	slow = second_half;
+	second_half = NULL;
+	while (slow != NULL)
+	{
+		listint_t *temp = slow->next;
+		slow->next = second_half;
+		second_half = slow;
+		slow = temp;
+	}
+
+	if (mid != NULL)
+	{
+		prev_slow->next = mid;
+		mid->next = second_half;
+	}
+	else
+		prev_slow->next = second_half;
+
+	return (is_pal);
 }
