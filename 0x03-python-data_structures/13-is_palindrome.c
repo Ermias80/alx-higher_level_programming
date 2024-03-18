30d90b126ec5070f168306b5f5d8b08b622f09ca
Ermias80 <ermiasmaz8069@gmail.com>
2024-03-18 14:27:15 -0700

    all

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,48 +1,53 @@
 #include "lists.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * is_palindrome - Checks if a singly linked list is a palindrome.
- * @head: Pointer to the head of the list
- * Return: 1 if the list is a palindrome, 0 otherwise
- */
+*add_nodeint - adds a new node at the beginning of a listint_t list
+*@head: head of listint_t
+*@n: int to add in listint_t list
+*Return: address of the new element, or NULL if it failed
+*/
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
+/**
+*is_palindrome - identify if a syngle linked list is palindrome
+*@head: head of listint_t
+*Return: 1 if it is palindrome else 0
+*/
 int is_palindrome(listint_t **head)
 {
-    listint_t *slow = *head, *fast = *head;
-    listint_t *prev_slow = NULL, *mid = NULL;
-    int is_pal = 1;
+	listint_t *head2 = *head;
+	listint_t *aux = NULL, *aux2 = NULL;
 
-    if (*head == NULL || (*head)->next == NULL)
-        return (1);
-
-    while (fast && fast->next)
-    {
-        fast = fast->next->next;
-
-        prev_slow = slow;
-        slow = slow->next;
-    }
-
-    if (fast != NULL)
-    {
-        mid = slow;
-        slow = slow->next;
-    }
-
-    listint_t *second_half = slow;
-    prev_slow->next = NULL;
-    reverse_list(&second_half);
-    is_pal = compare_lists(*head, second_half);
-    reverse_list(&second_half);
-
-    if (mid != NULL)
-    {
-        prev_slow->next = mid;
-        mid->next = second_half;
-    }
-    else
-        prev_slow->next = second_half;
-
-    return (is_pal);
+	if (*head == NULL || head2->next == NULL)
+		return (1);
+	while (head2 != NULL)
+	{
+		add_nodeint(&aux, head2->n);
+		head2 = head2->next;
+	}
+	aux2 = aux;
+	while (*head != NULL)
+	{
+		if ((*head)->n != aux2->n)
+		{
+			free_listint(aux);
+			return (0);
+		}
+		*head = (*head)->next;
+		aux2 = aux2->next;
+	}
+	free_listint(aux);
+	return (1);
 }
