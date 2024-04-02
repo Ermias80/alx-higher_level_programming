52e71a001e97c5bfa0544d7fe6a6149e886a9eb9
Ermias80 <ermiasmaz8069@gmail.com>
2024-04-02 09:19:30 -0700

    all

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""This module defines a Square class."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0):
+        """Initializes the Square instance."""
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+    @property
+    def size(self):
+        """Retrieve the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Calculates the area of the square."""
+        return self.__size ** 2
