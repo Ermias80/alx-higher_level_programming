6f993c304577874c41fd129e3015dbc0955a1fe0
Ermias80 <ermiasmaz8069@gmail.com>
2024-04-02 07:02:34 -0700

    all

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module defines an empty class Square.
+"""
+
+
+class Square:
+    """
+    This class represents a square.
+    """
+    pass
+
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+This is the solution of 0x06-python-classes
