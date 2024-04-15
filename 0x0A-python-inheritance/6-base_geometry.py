d38fe8506782a1eeede1cdb24e4f69f8beb1e041
Ermias80 <ermiasmaz8069@gmail.com>
2024-04-15 14:16:06 -0700

    all

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for Base Geometry"""
+
+class BaseGeometry:
+    """A base class representing geometry."""
+
+    def area(self):
+        """Raises an Exception since area method is not implemented."""
+        raise Exception("area() is not implemented")
