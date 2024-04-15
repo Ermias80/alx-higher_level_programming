09ad28dbd83c6d3b60a51022ddce2a40ed0f7b6c
Ermias80 <ermiasmaz8069@gmail.com>
2024-04-15 15:05:27 -0700

    all

--- 0x0A-python-inheritance/101-main.py
+++ 0x0A-python-inheritance/101-main.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-""" add_attribute module """
+add_attribute = __import__('101-add_attribute').add_attribute
 
+class MyClass():
+    pass
 
-def add_attribute(prmObject, prmName, prmValue):
-    """ add_attribute function """
-    if not hasattr(prmObject, "__dict__"):
-        raise TypeError("can't add new attribute")
-    if (not hasattr(prmObject, prmName)):
-        prmObject.__setattr__(prmName, prmValue)
+mc = MyClass()
+add_attribute(mc, "name", "John")
+print(mc.name)
+
+try:
+    a = "My String"
+    add_attribute(a, "name", "Bob")
+    print(a.name)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
