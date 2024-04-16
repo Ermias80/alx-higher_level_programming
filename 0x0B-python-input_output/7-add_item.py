c184d5e23a2361a1061a6f3b2f35b4e3cc57cf27
Ermias80 <ermiasmaz8069@gmail.com>
2024-04-16 10:17:24 -0700

    add item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,8 +3,10 @@
     This script adds all arguments to a Python list and saves them to a file
 """
 
+
 from sys import argv
 from os.path import exists
+
 
 if __name__ == "__main__":
     save = __import__('5-save_to_json_file').save_to_json_file
