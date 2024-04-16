602fa159993099fc95fec0bd8f9574746c41739d
Ermias80 <ermiasmaz8069@gmail.com>
2024-04-16 11:38:56 -0700

    all

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -5,15 +5,13 @@
 
 class Student:
     """Class that defines a student"""
-
     def __init__(self, first_name, last_name, age):
         """Initializes a student with first name, last name, and age."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-
     def to_json(self, attrs=None):
-        """Retrieves a dictionary representation of a Student instance.
+    """Retrieves a dictionary representation of a Student instance.
 
         Args:
             attrs (list): A list of strings specifying attribute names to retrieve.
