7e86af1144b43b04c385d68d0b19fcdc2c7238b2
Ermias80 <ermiasmaz8069@gmail.com>
2024-04-16 09:36:41 -0700

    write file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""Write file module"""
+
+
 def write_file(filename="", text=""):
-    """Writes a string to a text file (UTF8) and returns the number of characters written."""
+     """write_file
+
+    writes to a file
+
+    Args:
+        filename (file): file object to write to
+        text (str): test to write
+    """
     with open(filename, 'w', encoding='utf-8') as file:
         return file.write(text)
