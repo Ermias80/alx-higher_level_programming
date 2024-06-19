bbe6254874b63dedbe1081dc21495e4b72d2aa56
Ermias80 <ermiasmaz8069@gmail.com>
2024-06-19 13:42:51 -0700

    My JS

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+class Rectangle {
+  /**
+   * Define an Rectangle with dimensions
+   * @param {Number} w The value of the width
+   * @param {Number} h The value of the height
+   */
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
